0a17a6a6db6032ef68022b2ca806e545bae45692
Abimbola Idowu <abimbola2010@gmail.com>
2015-09-27 21:20:18 +0100

    add more cli commands

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,14 @@
 'use strict';
 
-let program = require('commander');
+import program from 'commander';
 import {slackHistoryExport} from './index.js';
 program
   .version('0.0.1')
   .usage('[options] <file ...>')
   .option('-t, --token <value>', '[REQUIRED] Enter your slack token API, you can generate it from here https://api.slack.com/web')
   .option('-u, --username [value]', 'Enter username to download history')
+  .option('-d, --directory [value]', 'Directory to save generated file')
+  .option('-f, --filename [value]', 'Name of generated file. Default is "<current timestamp><username || channel || group>-slack-history" e.g "1443378584156-abimbola-slack-history.json"')
   .parse(process.argv);
 
 if (!program.token) {
