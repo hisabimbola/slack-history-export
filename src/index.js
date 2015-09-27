629a961e7f3a9f07d1d391b4845653ed1a89798c
Abimbola Idowu <abimbola2010@gmail.com>
2015-09-27 21:21:11 +0100

    method to process user input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
+#! /usr/bin/env node
 'use strict';
-class Test {
-  test() {
-    return "test";
+
+import {processIM} from './commons.js';
+import jsonfile from 'jsonfile';
+import PleasantProgress from 'pleasant-progress';
+jsonfile.spaces = 4;
+export function slackHistoryExport(args) {
+  const progress = new PleasantProgress();
+  progress.start('working');
+  if(args.username) {
+    processIM(args.token, args.username).then(history => {
+      let currentDir = args.directory || process.cwd();
+      let filePath = (args.filename) ? `${currentDir}/${args.filename}.json` : `${currentDir}/${Date.now()}-${args.username}-slack-history.json`;
+      jsonfile.writeFile(filePath, history, function(err) {
+        if (!err) {
+          progress.stop();
+          console.log(`Done! file saved at ${filePath}`);
+        } else {
+          throw err;
+        }
+      });
+    }).catch(error => {
+      console.log('error', error);
+      progress.stop();
+      process.exit(1);
+    });
   }
 }
-
-var test = new Test();
-
-test.test();
