d1c2169e9a93409ea14cfbab5c50b104d62f2e03
Abimbola Idowu <abimbola2010@gmail.com>
2015-09-28 09:26:08 +0100

    fix error in lodash

--- bin/cli.js
+++ bin/cli.js
@@ -4,11 +4,11 @@
 var fs = require('fs');
 
 // checks for available update and returns an instance
-var defaults = require('lodash');
+var _ = require('lodash');
 var pkg = JSON.parse(fs.readFileSync(__dirname + '/../package.json'));
 
 require('update-notifier')({
-  pkg: defaults(pkg, { version: '0.0.0' }),
+  pkg: _.defaults(pkg, { version: '0.0.0' }),
 }).notify();
 
 require('./../lib/cli.js');
