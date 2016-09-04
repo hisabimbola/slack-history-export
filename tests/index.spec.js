119a8fd871dcc8b8cf1846da0f2d74bb3bab8b6a
Abimbola Idowu <abimbola2010@gmail.com>
2016-09-04 16:55:50 +0200

    feat(TEST): Add mocha and should for testing
    Add mocha and shouldjs for testing.
    Ref #21

---
+++ tests/index.spec.js
@@ -0,0 +1,10 @@
+
+import assert from 'assert'
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
